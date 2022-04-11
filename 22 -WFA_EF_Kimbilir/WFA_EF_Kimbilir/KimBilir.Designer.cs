cc6a160c5f5a8a86faed4b6864d8efce3f925154
EnesSERENLI <enes.serenli@hotmail.com>
2022-04-11 23:40:17 +0300

    Kimbilir - EntityFramework

---
+++ 22 -WFA_EF_Kimbilir/WFA_EF_Kimbilir/KimBilir.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WFA_EF_Kimbilir
+{
+    partial class KimBilir
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSayac = new System.Windows.Forms.Label();
+            this.btnSec = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lblSayac
+            // 
+            this.lblSayac.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSayac.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSayac.Location = new System.Drawing.Point(731, 12);
+            this.lblSayac.Name = "lblSayac";
+            this.lblSayac.Size = new System.Drawing.Size(318, 206);
+            this.lblSayac.TabIndex = 8;
+            this.lblSayac.Text = "30";
+            this.lblSayac.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSec
+            // 
+            this.btnSec.Location = new System.Drawing.Point(334, 358);
+            this.btnSec.Name = "btnSec";
+            this.btnSec.Size = new System.Drawing.Size(378, 48);
+            this.btnSec.TabIndex = 7;
+            this.btnSec.Text = "SEÇ";
+            this.btnSec.UseVisualStyleBackColor = true;
+            this.btnSec.Click += new System.EventHandler(this.btnSec_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(316, 394);
+            this.listBox1.TabIndex = 6;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(334, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(378, 340);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "AD";
+            this.columnHeader1.Width = 135;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "SOYAD";
+            this.columnHeader2.Width = 124;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "PUAN";
+            this.columnHeader3.Width = 259;
+            // 
+            // KimBilir
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1073, 442);
+            this.Controls.Add(this.lblSayac);
+            this.Controls.Add(this.btnSec);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "KimBilir";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.KimBilir_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSayac;
+        private System.Windows.Forms.Button btnSec;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
