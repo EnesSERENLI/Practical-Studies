2e92124f91fa8fe54e5e59172fe434a1dada613b
EnesSERENLI <enes.serenli@hotmail.com>
2022-04-12 20:53:56 +0300

    Northwind - EF -Sorgular

---
+++ 23 -WFA_EF_Northwind/WFA_EF_Northwind/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WFA_EF_Northwind
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.kategoriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ürünToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.müşteriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çalışanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.siparişToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tedarikçiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nakliyeciToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.raporlamaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kategoriToolStripMenuItem,
+            this.ürünToolStripMenuItem,
+            this.müşteriToolStripMenuItem,
+            this.çalışanToolStripMenuItem,
+            this.siparişToolStripMenuItem,
+            this.tedarikçiToolStripMenuItem,
+            this.nakliyeciToolStripMenuItem,
+            this.raporlamaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // kategoriToolStripMenuItem
+            // 
+            this.kategoriToolStripMenuItem.Name = "kategoriToolStripMenuItem";
+            this.kategoriToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.kategoriToolStripMenuItem.Text = "Kategori";
+            this.kategoriToolStripMenuItem.Click += new System.EventHandler(this.kategoriToolStripMenuItem_Click);
+            // 
+            // ürünToolStripMenuItem
+            // 
+            this.ürünToolStripMenuItem.Name = "ürünToolStripMenuItem";
+            this.ürünToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.ürünToolStripMenuItem.Text = "Ürün";
+            this.ürünToolStripMenuItem.Click += new System.EventHandler(this.ürünToolStripMenuItem_Click);
+            // 
+            // müşteriToolStripMenuItem
+            // 
+            this.müşteriToolStripMenuItem.Name = "müşteriToolStripMenuItem";
+            this.müşteriToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.müşteriToolStripMenuItem.Text = "Müşteri";
+            this.müşteriToolStripMenuItem.Click += new System.EventHandler(this.müşteriToolStripMenuItem_Click);
+            // 
+            // çalışanToolStripMenuItem
+            // 
+            this.çalışanToolStripMenuItem.Name = "çalışanToolStripMenuItem";
+            this.çalışanToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.çalışanToolStripMenuItem.Text = "Çalışan";
+            this.çalışanToolStripMenuItem.Click += new System.EventHandler(this.çalışanToolStripMenuItem_Click);
+            // 
+            // siparişToolStripMenuItem
+            // 
+            this.siparişToolStripMenuItem.Name = "siparişToolStripMenuItem";
+            this.siparişToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.siparişToolStripMenuItem.Text = "Sipariş";
+            this.siparişToolStripMenuItem.Click += new System.EventHandler(this.siparişToolStripMenuItem_Click);
+            // 
+            // tedarikçiToolStripMenuItem
+            // 
+            this.tedarikçiToolStripMenuItem.Name = "tedarikçiToolStripMenuItem";
+            this.tedarikçiToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.tedarikçiToolStripMenuItem.Text = "Tedarikçi";
+            this.tedarikçiToolStripMenuItem.Click += new System.EventHandler(this.tedarikçiToolStripMenuItem_Click);
+            // 
+            // nakliyeciToolStripMenuItem
+            // 
+            this.nakliyeciToolStripMenuItem.Name = "nakliyeciToolStripMenuItem";
+            this.nakliyeciToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.nakliyeciToolStripMenuItem.Text = "Nakliyeci";
+            this.nakliyeciToolStripMenuItem.Click += new System.EventHandler(this.nakliyeciToolStripMenuItem_Click);
+            // 
+            // raporlamaToolStripMenuItem
+            // 
+            this.raporlamaToolStripMenuItem.Name = "raporlamaToolStripMenuItem";
+            this.raporlamaToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.raporlamaToolStripMenuItem.Text = "Raporlama";
+            this.raporlamaToolStripMenuItem.Click += new System.EventHandler(this.raporlamaToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem kategoriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ürünToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem müşteriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çalışanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem siparişToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tedarikçiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nakliyeciToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem raporlamaToolStripMenuItem;
+    }
+}
+
---
+++ 23 -WFA_EF_Northwind/WFA_EF_Northwind/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WFA_EF_Northwind.Froms;
+
+namespace WFA_EF_Northwind
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void ChildForm(Form _childForm)
+        {
+            this.Width = _childForm.Width + 5;
+            this.Height = _childForm.Height + 30;
+            bool durum = false;
+
+            foreach (Form frm in this.MdiChildren)
+            {
+                if (frm.Text == _childForm.Text)
+                {
+                    durum = true;
+                    frm.Activate();
+                }
+                else
+                {
+                    frm.Close();
+                }
+
+            }
+
+            if (durum == false)
+            {
+                _childForm.MdiParent = this;
+                _childForm.StartPosition = FormStartPosition.Manual;
+                _childForm.FormBorderStyle = FormBorderStyle.None;
+                _childForm.Show();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kategoriToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm(new CategoryForm());
+        }
+
+        private void ürünToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm(new ProductForm());
+        }
+
+        private void raporlamaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm(new ReportForm());
+        }
+
+        private void müşteriToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm(new CustomerForm());
+        }
+
+        private void çalışanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm(new EmployeeForm());  
+        }
+
+        private void siparişToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm(new OrderForm());
+        }
+
+        private void tedarikçiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm(new SupplierForm());
+        }
+
+        private void nakliyeciToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm(new ShipperForm());
+        }
+    }
+}
