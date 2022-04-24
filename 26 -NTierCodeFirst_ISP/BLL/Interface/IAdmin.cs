d07d6722d02cd1fde3e648f7747cd0082de4416e
EnesSERENLI <enes.serenli@hotmail.com>
2022-04-24 23:11:13 +0300

    NtierCodeFirst - Solid ISP

---
+++ 26 -NTierCodeFirst_ISP/BLL/Interface/IAdmin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Interface
+{
+    public interface IAdmin
+    {
+        /// <summary>
+        /// Shows how many types of products are in a category.
+        /// </summary>
+        void ReportProductWithCategory();
+        /// <summary>
+        /// Lists the products between the two specified prices.
+        /// </summary>
+        /// <param name="price1"></param>
+        /// <param name="price2"></param>
+        void ReportProductWithPrice(int price1, int price2);
+
+        /// <summary>
+        /// Lists the products over the specified stock amount.
+        /// </summary>
+        /// <param name="stock"></param>
+        void ReportProductWithStock(int stock);
+    }
+}
---
+++ 26 -NTierCodeFirst_ISP/BLL/Interface/IAppUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Interface
+{
+    public interface IAppUser
+    {
+        void CategoryList();
+        void ProductList();
+    }
+}
